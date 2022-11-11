73c675a6dbd6b0e29556ca4606cf42f272953b62
Protonm0KEN <109662775+Protonm0KEN@users.noreply.github.com>
2022-11-12 00:35:42 +0500

    project

--- script.js
+++ script.js
@@ -27,8 +27,32 @@ class Scroll {
         }
     }
 }
-
+class trollGame {
+    constructor(obj){
+        this.element = document.querySelector(obj.element)
+        this.element2 = document.querySelector(obj.element2)
+        this.element.style.position = 'absolute'
+        this.element.addEventListener('click', () => {
+            this.element.style.left = this.randomNumbersX() + 'px'
+            this.element.style.top = this.randomNumbersY() + 'px'
+        })
+    }
+    randomNumbersX(min, max){
+        min = 50
+        max = this.element2.offsetWidth
+        return Math.random() * (max - min) + min;
+    }
+    randomNumbersY(min, max){
+        min = 50
+        max = this.element2.offsetHeight
+        return Math.random() * (max - min) + min;
+    }
+}
 let scroll = new Scroll({
     element: '.header__nav',
     top: 100,
+})
+let random = new trollGame({
+    element: '.header__content',
+    element2: '.header'
 })
